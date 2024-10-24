2c77238bf1f22600f665939437a541f466e3301c
PriyanshuPatel23 <pkp266276@gmail.com>
2024-10-24 15:40:10 +0530

    deprecation issue resolved

--- app.js
+++ app.js
@@ -3,55 +3,49 @@ const app = express();
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const mongoose = require("mongoose");
-const usersRoutes = require('./routes/users-routes');
-const {Account} = require("./models/index");
+const usersRoutes = require("./routes/users-routes");
+const { Account } = require("./models/index");
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 //Connecting to mongodb server and storing ip_data
-mongoose
-  .connect(process.env.MONGODB_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    })
-  .then(
-    () => console.log('DB connection success'),
-    (err) => console.log('Error connecting to DB', err)
-  )
+mongoose.connect(process.env.MONGODB_URI, {}).then(
+  () => console.log("DB connection success"),
+  (err) => console.log("Error connecting to DB", err)
+);
 
 app.use(express.json());
 
-app.use(require("express-session")({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
+app.use(
+  require("express-session")({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 //Passport Configuration
 app.use(passport.initialize());
 app.use(passport.session());
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
-passport.use(Account.createStrategy())
-
+passport.use(Account.createStrategy());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  next()
-})
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
-app.use("/", usersRoutes)
+app.use("/", usersRoutes);
 
-const PORT = process.env.PORT||3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log(`Serving app on port ${PORT}`);
 });
